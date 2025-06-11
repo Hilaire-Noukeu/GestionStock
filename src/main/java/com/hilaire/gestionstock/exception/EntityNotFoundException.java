6dd7286115bb82295d7913eb82fdde867dcc188f
Hilaire-Noukeu <79503776+Hilaire-Noukeu@users.noreply.github.com>
2025-06-11 15:09:12 +0100

    structure de la base de donnees modifiee

--- src/main/java/com/hilaire/gestionstock/exception/EntityNotFoundException.java
+++ src/main/java/com/hilaire/gestionstock/exception/EntityNotFoundException.java
@@ -17,6 +17,7 @@ public class EntityNotFoundException extends RuntimeException {
 
     }
     public EntityNotFoundException(String message, Throwable cause) {
+
         super(message, cause);
     }
     public EntityNotFoundException(String message, Throwable cause, ErrorCodes errorCode) {
---
+++ src/main/java/com/hilaire/gestionstock/handers/ErrorDto.java
@@ -0,0 +1,21 @@
+package com.hilaire.gestionstock.handers;
+
+import com.hilaire.gestionstock.exception.ErrorCodes;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private int httpCode;
+    private ErrorCodes errorCode;
+    private String message;
+    private List<String> errors = new ArrayList<>();
+
+}
